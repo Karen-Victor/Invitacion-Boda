3c6d1397cc1799ea85b52073b84aa495a98a7768
Karen-Victor <karenyvictor14102023@gmail.com>
2024-10-20 23:01:17 -0500

    Actualización página

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function Init(){
+
+    function calcularTiempoRestante() {
+        const ahora = new Date();
+        const fechaObjetivo = new Date('2024-11-09T16:00:00');
+        const diferencia = fechaObjetivo - ahora;
+        if(diferencia>0){
+            const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+            const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+            const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+            
+            spanContadorDias.innerText = dias;
+            spanContadorHoras.innerText = horas;
+            spanContadorMinutos.innerText = minutos;
+            spanContadorSegundos.innerText = segundos;
+        }
+    }
+    
+    calcularTiempoRestante();
+    setInterval(calcularTiempoRestante,1000);
 }
 Init();
